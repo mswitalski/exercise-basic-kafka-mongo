4f89ee19c4d30963d01b2f4793eaea58bacfb196
Marek Świtalski <33295355+mswitalski@users.noreply.github.com>
2019-05-27 14:20:22 +0200

    Repository init

---
+++ kafka-loader/src/main/java/mswitalski/exercises/basickafkamongo/kafkaloader/KafkaLoaderApp.java
@@ -0,0 +1,19 @@
+package mswitalski.exercises.basickafkamongo.kafkaloader;
+
+/**
+ * Application responsible for receiving data from chosen database
+ * and loading it into chosen message broker.
+ *
+ * At the moment the following integrations are available:
+ *   - Message brokers:
+ *     * none
+ *
+ *   - Databases:
+ *     * none
+ */
+public class KafkaLoaderApp {
+
+  public static void main(String... args) {
+    System.out.println("Hello world from Kafka Loader App!");
+  }
+}
---
+++ mongo-loader/src/main/java/mswitalski/exercises/basickafkamongo/mongoloader/MongoLoaderApp.java
@@ -0,0 +1,19 @@
+package mswitalski.exercises.basickafkamongo.mongoloader;
+
+/**
+ * Application responsible for receiving data from chosen message broker
+ * and persisting it in chosen database.
+ *
+ * At the moment the following integrations are available:
+ *   - Message brokers:
+ *     * none
+ *
+ *   - Databases:
+ *     * none
+ */
+public class MongoLoaderApp {
+
+  public static void main(String... args) {
+    System.out.println("Hello world from Mongo Loader App!");
+  }
+}
