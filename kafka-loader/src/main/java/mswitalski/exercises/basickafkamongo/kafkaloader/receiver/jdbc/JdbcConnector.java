2a27d7bcc8511af384ca6d11c70cb8980664632d
Marek Świtalski <33295355+mswitalski@users.noreply.github.com>
2019-05-29 13:36:47 +0200

    Deleted unnecessary DriverManagerWrapper

--- kafka-loader/src/main/java/mswitalski/exercises/basickafkamongo/kafkaloader/receiver/jdbc/JdbcConnector.java
+++ kafka-loader/src/main/java/mswitalski/exercises/basickafkamongo/kafkaloader/receiver/jdbc/JdbcConnector.java
@@ -3,6 +3,7 @@ package mswitalski.exercises.basickafkamongo.kafkaloader.receiver.jdbc;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Objects;
 import java.util.Properties;
@@ -10,30 +11,21 @@ import java.util.Properties;
 @Slf4j
 public class JdbcConnector {
 
-    private String dbUrl;
+    private final String databaseUrl;
     private final Properties properties;
-    private DriverManagerWrapper driverManager;
 
     public JdbcConnector(String databaseUrl, Properties properties) {
-        this.dbUrl = Objects.requireNonNull(databaseUrl);
+        this.databaseUrl = Objects.requireNonNull(databaseUrl);
         this.properties = Objects.requireNonNull(properties);
-        this.driverManager = DriverManagerWrapper.INSTANCE;
-    }
-
-    JdbcConnector(String databaseUrl, Properties properties, DriverManagerWrapper driverManager) {
-        this.dbUrl = Objects.requireNonNull(databaseUrl);
-        this.properties = Objects.requireNonNull(properties);
-        this.driverManager = Objects.requireNonNull(driverManager);
     }
 
     Connection getConnection() throws SQLException {
         Connection conn;
         if (properties.isEmpty()) {
-            conn = driverManager.getConnection(dbUrl);
+            conn = DriverManager.getConnection(databaseUrl);
         } else {
-            conn = driverManager.getConnection(dbUrl, properties);
+            conn = DriverManager.getConnection(databaseUrl, properties);
         }
-        conn.setAutoCommit(false);
 
         return conn;
     }
